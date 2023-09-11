5d03e8e060ba69238d18d29b2f0b0c7caaa43de2
Haniel <Haniel20008@gmail.com>
2023-09-11 14:43:22 -0400

    Added name as a variable

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val myName = "Haniel";
         setContentView(R.layout.activity_main)
+        val myName = "Haniel"
+        val textView = findViewById<TextView>(R.id.textView)
+        val welcomeMessage = getString(R.string.welcome_message, myName)
+        textView.text = welcomeMessage
     }
 }
